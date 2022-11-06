1f09782f6ad20ba502c6c3bb3029e05a74dc2a07
Malfrador <malfrador@gmail.com>
2022-11-06 19:14:55 +0100

    Add first AI stuff

---
+++ Assets/GlobalScripts/entity/ai/AbstractAIGoal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.AI;
+
+namespace GlobalScripts.entity.ai {
+    public abstract class AbstractAIGoal {
+
+        protected DustEntity Entity;
+        protected NavMeshAgent Agent;
+        
+        protected AbstractAIGoal(DustEntity entity) {
+            Entity = entity;
+            Agent = entity.GetComponent<NavMeshAgent>();
+        }
+
+        public abstract void OnStart();
+
+        public abstract void Run();
+
+        public abstract void OnStop();
+
+        public abstract void Cancel();
+    }
+}
---
+++ Assets/GlobalScripts/entity/ai/GuardPositionGoal.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace GlobalScripts.entity.ai {
+    public class GuardPositionGoal : AbstractAIGoal {
+
+        private Vector3 _guardPosition;
+        private float _maxDistance = 5f;
+
+        public GuardPositionGoal(DustEntity entity) : base(entity) {
+            
+        }
+
+        public override void OnStart() {
+          
+        }
+
+        public override void Run() {
+            var pos = Entity.gameObject.transform.position;
+            if (Vector3.Distance(pos, _guardPosition) > _maxDistance) {
+                Agent.destination = _guardPosition;
+                return;
+            }
+            Vector3 target;
+            target.x = +Random.Range(-1.5f, 1.5f);
+            target.z = +Random.Range(-1.5f, 1.5f);
+            target.y = pos.y;
+            Debug.Log("Moving to " + target);
+            Agent.destination = target;
+        }
+
+        public override void OnStop() {
+            
+        }
+
+        public override void Cancel() {
+            Agent.destination = _guardPosition;
+        }
+    }
+}
