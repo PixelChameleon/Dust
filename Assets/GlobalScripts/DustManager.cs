aa1659da8f43722f2380e84c30185f5a2a0fb9da
Malfrador <malfrador@gmail.com>
2023-01-07 12:14:45 +0100

    add level switching

--- Assets/GlobalScripts/DustManager.cs
+++ Assets/GlobalScripts/DustManager.cs
@@ -6,13 +6,13 @@ namespace GlobalScripts {
     public class DustManager : MonoBehaviour {
 
         public List<ItemStack> ItemRegistry = new();
-
-        private int _currentSceneID = 1;
+        private int _currentSceneID = 2;
         public PlayerScript player;
         public GameObject bullet;
         public Sprite[] ItemSprites;
 
         private void Start() {
+            
             SceneManager.LoadScene(2, LoadSceneMode.Additive);
             ItemRegistry.Add(new ItemStack(0, "Key", ItemSprites[0]));
             if (!Application.isEditor) { // Scene loading behaves differently in the editor for some reason...
@@ -24,11 +24,9 @@ namespace GlobalScripts {
 
         // Switches the scene. Scene IDs are set in File -> Build Settings
         public void SwitchScene(int id) {
-            player.camera.enabled = false;
             SceneManager.UnloadScene(_currentSceneID);
             SceneManager.LoadScene(id, LoadSceneMode.Additive); // Maybe async loading? although they are pretty small anyways
             _currentSceneID = id;
-            player.camera.enabled = true;
             Debug.Log("Switched scene to " + _currentSceneID);
         }
     }
