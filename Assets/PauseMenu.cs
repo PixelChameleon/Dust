4d2e4815f8382209dd88eec36cee042f76bff1de
Korayd25 <koray.dinc@gmx.de>
2023-01-13 12:26:03 +0100

    PauseMenu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+     void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if ( GameIsPaused)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
